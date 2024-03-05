00f15a4cd0307eaed6266626520371f4aa1726a7
Yunhong Min <yunhongmin00@gmail.com>
2024-03-05 18:30:48 +0900

    First commit

---
+++ src/main/java/com/yunhongmin/codi/CodiApplication.java
@@ -0,0 +1,11 @@
+package com.yunhongmin.codi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CodiApplication.class, args);
+	}
+}
